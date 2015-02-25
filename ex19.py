cb7b06453c1d87efdfa65a189a86c9c1ffaa13e7
Matthew Wong <Matthew@Matthews-MacBook-Pro.local>
2015-02-25 01:40:56 -0500

    Learning Python
    Learning python from http://learnpythonthehardway.org/book/

---
+++ ex19.py
@@ -0,0 +1,27 @@
+def cheese_and_crackers(cheese_count, boxes_of_crackers):
+    print "You have %d cheeses!" % cheese_count
+    print "You have %d boxes of crackers!" % boxes_of_crackers
+    print "Man that's enough for a party!"
+    print "Get a blanket.\n"
+
+print "We can just give the function numbers directly:"
+cheese_and_crackers(20, 30)
+
+print "OR, we can use variables from our script:"
+amount_of_cheese = 10
+amount_of_crackers = 50
+
+cheese_and_crackers(amount_of_cheese, amount_of_crackers)
+
+print "We can even do math inside too:"
+cheese_and_crackers(10 + 20, 5 + 6)
+
+print "And we can combine the two, variables and math:"
+cheese_and_crackers(amount_of_cheese + 100, amount_of_crackers + 1000)
+
+your_cheese = int(raw_input("How many cheese would you like?\n>"))
+your_cracker = int(raw_input("How many boxes of crackers would you like?\n>"))
+
+print "You have %d cheese" % your_cheese
+print "You have %d boxes of crackers" % your_cracker
+print "That may or may not be enough for a party, but it's what you want"
